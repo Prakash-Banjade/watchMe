faf0d10535e009a40837bebf8809edd14d92415e
Prakash Banjade <prakashbanjade191@gmail.com>
2024-04-21 23:52:00 +0545

    modified nepse scraping, added some extra data

--- server/src/sharemarket/sharemarket.service.ts
+++ server/src/sharemarket/sharemarket.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
 import { brightDataAxiosOptions } from 'src/config/brightData.config';
 import * as cheerio from 'cheerio'
@@ -8,31 +7,29 @@ import * as cheerio from 'cheerio'
 @Injectable()
 export class SharemarketService {
 
-  constructor(
-    private readonly configService: ConfigService
-  ) { }
-
   async scrapeAll() {
 
     const response = await axios.get(`https://merolagani.com/LatestMarket.aspx`, brightDataAxiosOptions);
 
     const $ = cheerio.load(response.data);
 
-    const data = $('#ctl00_ContentPlaceHolder1_LiveTrading > table tbody tr').map((index, element) => {
+    const stocks = $('#ctl00_ContentPlaceHolder1_LiveTrading > table tbody tr').map((index, element) => {
       const columns = $(element).find('td');
       return {
         symbol: $(columns[0]).find('a').text().trim(),
-        LTP: $(columns[1]).text().trim(),
-        percentChange: $(columns[2]).text().trim(),
-        open: $(columns[3]).text().trim(),
-        high: $(columns[4]).text().trim(),
-        low: $(columns[5]).text().trim(),
-        quantity: $(columns[6]).text().trim(),
+        url: `https://merolagani.com/${$(columns[0]).find('a').attr('href')}`,
+        title: $(columns[0]).find('a').attr('title'),
+        LTP: +$(columns[1]).text().trim().replace(/,/g, ""),
+        percentChange: +$(columns[2]).text().trim(),
+        open: +$(columns[3]).text().trim().replace(/,/g, ""),
+        high: +$(columns[4]).text().trim().replace(/,/g, ""),
+        low: +$(columns[5]).text().trim().replace(/,/g, ""),
+        quantity: +$(columns[6]).text().trim().replace(/,/g, ""),
         pClose: $(columns[7]).text().trim(),
         diff: $(columns[8]).text().trim()
       };
     }).get();
 
-    return data;
+    return stocks
   }
 }
