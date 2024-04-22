e4c5424f705bdf40ada5a0ccf54fb30d1e98f2c1
Prakash Banjade <prakashbanjade191@gmail.com>
2024-04-22 23:08:19 +0545

    Add CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,9 @@ const PORT = process.env.PORT || 3001
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000']
+  });
   await app.listen(PORT).then(() => console.log(`Application is running on: ${PORT}`));
 }
 bootstrap();
