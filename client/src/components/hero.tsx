b9d2201ae00a7d0845c7a7f3f46f770d4d85b5cb
Prakash Banjade <prakashbanjade191@gmail.com>
2024-06-02 17:18:43 +0545

    fix hero animation

--- client/src/components/hero.tsx
+++ client/src/components/hero.tsx
@@ -11,24 +11,32 @@ export default function Hero({ }: Props) {
     const [choice, setChoice] = useState(false);
 
     return choice ? <Choice /> : (
-        <motion.main
-            initial={{ opacity: 0, y: 1000 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ type: 'spring', duration: 0.5 }}
-            className='min-h-screen relative overflow-hidden'
+        <div
+            className='min-h-screen grid place-items-center'
         >
-            <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-                <h1 className="sm:text-7xl text-5xl lg:text-9xl font-bold text-center relative z-20">
-                    Watch Me
-                </h1>
-                <p className='text-center'>Watch Market, Track Products, and Get Alerts.</p>
-                <div className='flex justify-center'>
-                    <Button size={'lg'} className='rounded-full mt-5 sm:text-xl md:px-8 md:py-6 shadow-lg hover:shadow-2xl transition-all' onClick={() => setChoice(true)}>
-                        Get started
-                    </Button>
+            <motion.main
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                    stiffness: 200,
+                    damping: 25,
+                    restDelta: 2,
+                    duration: .5
+                }}
+            >
+                <div>
+                    <h1 className="sm:text-7xl text-5xl lg:text-9xl font-bold text-center relative z-20">
+                        Watch Me
+                    </h1>
+                    <p className='text-center'>Watch Market, Track Products, and Get Alerts.</p>
+                    <div className='flex justify-center'>
+                        <Button size={'lg'} className='rounded-full mt-5 sm:text-xl md:px-8 md:py-6 shadow-lg hover:shadow-2xl transition-all' onClick={() => setChoice(true)}>
+                            Get started
+                        </Button>
+                    </div>
                 </div>
-            </div>
-        </motion.main>
+            </motion.main>
+        </div>
     )
 }
 
